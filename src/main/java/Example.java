ff4530a401fccbb443a21ccae07cfc5b8283cac0
dgupta <dgupta@ebi.ac.uk>
2023-04-14 13:37:37 +0100

    Test

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 public class Example {
     @RequestMapping("/")
     String home() {
-        return "Hello World - v3!";
+        return "Hello World - v31!";
     }
 
     public static void main(String[] args) {
